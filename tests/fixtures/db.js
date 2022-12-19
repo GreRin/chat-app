7fe5351e4f6b303a5b4ddfa763ec6e746914b440
GreRin <rigor1988@gmail.com>
2022-12-19 20:18:14 +0100

    feat: covered user.js with tests + added sceleton for socket.io

---
+++ tests/fixtures/db.js
@@ -0,0 +1,24 @@
+const users = [
+  {
+    id: '1',
+    username: 'greg',
+    room: 'js'
+  },
+  {
+    id: '2',
+    username: 'rick',
+    room: 'node'
+  },
+  {
+    id: '1',
+    username: 'nick',
+    room: 'js'
+  }
+]
+const user = {
+  id: '4',
+  username: 'greg',
+  room: 'js'
+}
+
+module.exports = { user, users }
